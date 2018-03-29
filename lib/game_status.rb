b1fe35db4d2372e6452fd3e645fcdcc06d726c9e
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-29 18:07:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,6 +20,9 @@ WIN_COMBINATIONS = [
 def won?(board)
   winning_combo = -1
   WIN_COMBINATIONS.each do |win_combo|
+      if winning_combo != -1 return winning_combo
+      end
+      
       win_index_1 = win_combo[0]
       win_index_2 = win_combo[1]
       win_index_3 = win_combo[2]
