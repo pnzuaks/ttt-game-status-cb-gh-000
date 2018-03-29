3b3ed76df44566ff9b8ed32adaaf30df0ac1b5b2
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-29 18:02:07 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,23 +27,25 @@ def won?(board)
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
       
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return win_combo
-      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        return win_combo
-    
-      else
+       if position_1 != "X" || "O" && position_2 != "X" || "O" && position_3 != "X" || "O"
         return false
-      end
+    else return win_combo
+    end
   end
 end
 
-   if position_1 != "X" && position_2 == "X" && position_3 == "X"
-        return win_combo
-  
-  
-  
 
+  
+  
+  
+# if position_1 == "X" && position_2 == "X" && position_3 == "X"
+#         return win_combo
+#       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+#         return win_combo
+    
+#       else
+#         return false
+#       end
 
 
 
