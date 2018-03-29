9ee342063850ac81d72699a23b883b676b2c6991
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-29 17:53:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,6 +31,7 @@ def won?(board)
         return win_combo
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combo
+    
       else
         return false
       end
